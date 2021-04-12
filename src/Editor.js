8b54badfbb13229b449b59d817d4aa80bd67d1ca
ardayuksel <ardayuksel@Ardas-MacBook-Air.local>
2021-04-13 00:24:05 +0300

    initial commit

---
+++ src/Editor.js
@@ -0,0 +1,11 @@
+const Editor = (props) => {
+  return (
+    <textarea
+      id="editor"
+      onChange={props.onChange}
+      type="text"
+      value={props.markdown}
+    />
+  );
+};
+export default Editor;
